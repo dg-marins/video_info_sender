5740049ce6bdebd6740632b646a29986b241899b
Douglas Marins <douglasmarinssouza@gmail.com>
2024-11-08 13:49:00 -0300

    Lógica que processa cada carro adicinada

--- app.py
+++ app.py
@@ -11,15 +11,18 @@ if __name__ == '__main__':
     api_config = config_file.get("api")
     host = api_config.get("api_host")
     port = api_config.get("api_port")
-    api_url = f"http://{host}:{port}"
+    api_url = f"{host}:{port}"
 
     api_sectrans = SectransAPI(api_url=api_url, api_token=config_file.get("api_token"))
 
     app_config = config_file.get("app")
 
+    print("Pegando carros na api ...")
     api_cars = api_sectrans.get_cars_by_company_id(app_config.get("servidor_id"))
+    
     server_cars = os.listdir(app_config.get("source_video_path"))
     
+    cars_to_process = []
     for server_car in server_cars:
         
         api_car_id = None
@@ -31,11 +34,16 @@ if __name__ == '__main__':
             print(f"ID do carro {server_car} não encontrada")
             continue
 
-        car_path = os.path.join(app_config.get("source_video_path"), server_car)
+        else:
+            data = {"id": api_car_id, "name": server_car}
+            cars_to_process.append(data)
 
-        videos_info = util.get_list_of_all_videos_info(os.path.join(app_config["source_video_path"], server_car))
+    for car in cars_to_process:
+        car_path = os.path.join(app_config.get("source_video_path"), car["name"])
+
+        print(f'[{car.get("name")}] Iniciando coleta de informações de vídeos.')
+        videos_info = util.get_list_of_all_videos_info(os.path.join(app_config["source_video_path"], car["name"]))
         
-        formater_send_data = util.get_formated_data_to_send(api_car_id, videos_info)
+        formater_send_data = util.get_formated_data_to_send(car["id"], videos_info)
         api_sectrans.send_videos_info(formater_send_data)
-        print("End")
-        # api_sectrans.send_video_info(video_info)
+        print(f'[{car["name"]}] End')
